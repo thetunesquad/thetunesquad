1d85e2f908d2af3b51c8dba67313c749d9fa6bb4
cleex1004 <cleex1004@gmail.com>
2017-02-27 13:33:57 -0800

    server.js set up

---
+++ server.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const pg = require('pg');
+const express = require('express');
+const request = require('request');
+const querystring = require('querystring');
+const app = express();
+const PORT = process.env.PORT || 7000;
+const conString = process.env.DATABASE_URL || 'postgres://localhost:5432';
+const client = new pg.Client(conString);
+client.connect();
+
+app.use(express.static('./public'));
+
+app.get('*', (request, response) => response.sendFile('index.html', {root: './public'}));
+
+app.listen(PORT, function() {
+  console.log(`Portfolio is being served at ${PORT}`);
+});
