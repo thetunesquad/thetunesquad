b104a2b0aa60d0c5d1e85a9409223f1001badce5
cleex1004 <cleex1004@gmail.com>
2017-02-28 15:41:14 -0800

    50 ids, not unique, need to combine arrays

--- public/scripts/model/spotify.js
+++ public/scripts/model/spotify.js
@@ -4,9 +4,15 @@ const search = {};
 
 const artist1artist2 = [];
 
-const artistId = [];
+const artist12Id = [];
 
-const relatedArtists = [];
+const relatedArtists12 = [];
+
+const relatedArtistsAJ = [];
+
+const artistsA1J5Id = [];
+
+let artistsAJId = [];
 
 var searchArtist1 = function (query1) {
   $.ajax({
@@ -18,7 +24,7 @@ var searchArtist1 = function (query1) {
     },
     success: function (response) {
       artist1artist2.push(response.artists.items);
-      artistId.push(response.artists.items[0].id);
+      artist12Id.push(response.artists.items[0].id);
     }
   });
 };
@@ -33,7 +39,7 @@ var searchArtist2 = function (query2) {
     },
     success: function (response) {
       artist1artist2.push(response.artists.items);
-      artistId.push(response.artists.items[0].id);
+      artist12Id.push(response.artists.items[0].id);
     }
   });
 };
@@ -43,33 +49,54 @@ document.getElementById('search-form').addEventListener('submit', function (e) {
   searchArtist1($('#query1').val());
   searchArtist2($('#query2').val());
   console.log(artist1artist2);
-  console.log(artistId);
+  console.log(artist12Id);
 }, false);
 
-function relatedArtist() {
-  artistId.forEach(function (id){
+function getRelated12() {
+  artist12Id.forEach(function (id){
     $.ajax({
       url: `https://api.spotify.com/v1/artists/${id}/related-artists`,
       success: function (response) {
-        //console.log(response);
-        relatedArtists.push(response.artists);
-        console.log(relatedArtists);
+        relatedArtists12.push(response.artists);
+        console.log(relatedArtists12);
       }
     })
   })
 }
 
-function unique() {
-  const AJ = [];
+function getUniqueAJ() {
+  let AJ = [];
   for (var i = 0; i < 10; i++) {
-    if(!AJ.includes(relatedArtists[0][i].id)) {
-      AJ.push(relatedArtists[0][i].id);
+    if(!AJ.includes(relatedArtists12[0][i].id)) {
+      AJ.push(relatedArtists12[0][i].id);
     }
-    if(!AJ.includes(relatedArtists[1][i].id)) {
-      AJ.push(relatedArtists[1][i].id);
+    if(!AJ.includes(relatedArtists12[1][i].id)) {
+      AJ.push(relatedArtists12[1][i].id);
     }
   }
-  let AJSliced =  AJ.slice(0, 10);
-  console.log(AJSliced);
-  return AJSliced;
+  artistsAJId = AJ.slice(0, 10);
+  console.log(artistsAJId);
+  //return artistsAJId;
+}
+
+function getRelatedAJ() {
+  artistsAJId.forEach(function (id){
+    $.ajax({
+      url: `https://api.spotify.com/v1/artists/${id}/related-artists`,
+      success: function (response) {
+        relatedArtistsAJ.push(response.artists);
+        console.log(relatedArtistsAJ);
+      }
+    })
+  })
+}
+
+function getUniqueA1J5() {
+  relatedArtistsAJ.forEach(function(element) {
+    let truncated = element.slice(0,5);
+    truncated.map(function(artist) {
+      artistsA1J5Id.push(artist.id);
+    })
+  })
+  console.log(artistsA1J5Id);
 }
