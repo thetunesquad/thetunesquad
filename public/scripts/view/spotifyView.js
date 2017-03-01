622a08f0afa8c6bb65f617c8c49c26510cc43975
Allie Grampa <agrampa@yahoo.com>
2017-03-01 12:58:23 -0800

    still working on song preview, still not working how we want it to

--- public/scripts/view/spotifyView.js
+++ public/scripts/view/spotifyView.js
@@ -28,3 +28,35 @@ spotifyView.resetButton = function() {
     $('#query2').empty();
   })
 };
+//ATTEMPT TO ADD CLICK EVENT BASED ON SPOTIFY'S EXAMPLE, NOT WORKING YET
+//EVERYTHING ATTEMPTED IN A CLICK EVENT IN .JS FILES OPENS PREVIEW_URL IN THE SAME WINDOW, THEN HITTING BACK BUTTON CLEARS OUT PLAYLIST
+
+// $('.icon-play3').on('click', function (e) {
+//   e.preventDefault();
+//   // var target = e.target;
+//   var songToPlay = this.preview_url;
+//   songToPlay.play();
+// });
+//   
+// //THIS IS SOME OF THE SOURCE CODE FROM SPOTIFY
+//   if (target !== null && target.classList.contains('cover')) {
+//     if (target.classList.contains(playingCssClass)) {
+//         audioObject.pause();
+//     } else {
+//       if (audioObject) {
+//         audioObject.pause();
+//       }
+//       fetchTracks(target.getAttribute('data-album-id'), function (data) {
+//         audioObject = new Audio(data.tracks.items[0].preview_url);
+//         audioObject.play();
+//         target.classList.add(playingCssClass);
+//         audioObject.addEventListener('ended', function () {
+//           target.classList.remove(playingCssClass);
+//         });
+//         audioObject.addEventListener('pause', function () {
+//           target.classList.remove(playingCssClass);
+//         });
+//       });
+//     }
+//   }
+// });
