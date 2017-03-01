0aa9d13fc8f1e212550f46e518953108aeb6912b
Allie Grampa <agrampa@yahoo.com>
2017-03-01 11:08:02 -0800

    still version 1 of preview popup, minor changes

--- public/scripts/view/spotifyView.js
+++ public/scripts/view/spotifyView.js
@@ -1,19 +1,11 @@
 'use strict';
 
-// let source = $('#playlist-template').html();
-// let templateRender = Handlebars.compile(source);
-//
-// trackData.forEach(track){
-//   templateRender(track);
-// };
-
 function u() {
   let render = Handlebars.compile($('#playlist-template').text());
   $('#playlist').append(
     trackData.map(render)
   );
-};
-
+}
 
 (function(module){
   const spotifyView = {};
@@ -24,8 +16,32 @@ function u() {
       $('#query1').empty();
       $('#query2').empty();
     })
-  }
-  
+  };
+
   module.spotifyView = spotifyView;
-  
+
 })(window);
+
+
+// let source = $('#playlist-template').html();
+// let templateRender = Handlebars.compile(source);
+//
+// trackData.forEach(track){
+//   templateRender(track);
+// };
+
+// 
+// spotifyView.previewButton = function() {
+//   $('.icon-play3').click(function(){
+//     $(this).play();
+//     // $(this).pause();
+//   })
+// };
+ 
+  // $('.icon-play3').on('click', function(){
+  //   preview = window.open(target='popup','{{preview_url}}', 'popup', 'width=150, height=50');
+  // })
+  // 
+  // $('.icon-play3').on('click', function(){
+  //   preview.close();
+  // })
