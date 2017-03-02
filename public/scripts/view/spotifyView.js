3ced131c407310663d7975ce4d28b448d0f8e27a
Alex Cahn <alexcahn@gmail.com>
2017-03-01 17:05:43 -0800

    got artist images, and removed undefined artists

--- public/scripts/view/spotifyView.js
+++ public/scripts/view/spotifyView.js
@@ -1,19 +1,10 @@
 'use strict';
 
-// let source = $('#playlist-template').html();
-// let templateRender = Handlebars.compile(source);
-//
-// trackData.forEach(track){
-//   templateRender(track);
-// };
-
 function u() {
+  $('#image1 img').attr('src', artist1artist2[0][0].images[0].url);
+  $('#image2 img').attr('src', artist1artist2[1][0].images[0].url);
   let render = Handlebars.compile($('#playlist-template').text());
   $('#playlist').append(
     trackData.map(render)
   );
-};
-
-$('#reset-button').on('click', function(){
-  
-})
+}
