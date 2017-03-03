c1d6ed0bf69d49122880e4f9471246e9a0cd9b74
Kyle Hillman <khillman84@gmail.com>
2017-03-03 00:44:58 -0800

    mobile first styling attempt

--- public/scripts/view/spotifyView.js
+++ public/scripts/view/spotifyView.js
@@ -1,8 +1,8 @@
 'use strict';
 
 function displayPlaylist() {
-  $('#image1 img').attr('src', artist1artist2[0][0].images[0].url);
-  $('#image2 img').attr('src', artist1artist2[1][0].images[0].url);
+  $('#image1').attr('src', artist1artist2[0][0].images[0].url);
+  $('#image2').attr('src', artist1artist2[1][0].images[0].url);
   let render = Handlebars.compile($('#playlist-template').text());
   $('#playlist').append(
     trackData.map(render)
