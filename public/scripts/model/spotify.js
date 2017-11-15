07bc10f2d54cce91f91a1af35c6390ac6e4fcff9
Allie <allie@codefellows.com>
2017-11-15 14:30:14 -0800

    fixing auth

--- public/scripts/model/spotify.js
+++ public/scripts/model/spotify.js
@@ -69,96 +69,96 @@
         }
       })
     })
-    .done(artist12Id.forEach(function (id){
-      $.ajax({
-        url: `https://api.spotify.com/v1/artists/${id}/related-artists`,
-        headers: {
-          'Authorization': 'Bearer bd56804a81f3496e909d25ae1ee01481',
-        },
-        async: false,
-        success: function (response) {
-          relatedArtists12.push(response.artists);
-          console.log('got related artists of original 2');
-        }
-      })
-    }))
-    .done(function(){
-      for (let i = 0; i < 10; i++) {
-        if(!AJ.includes(relatedArtists12[0][i].id)) {
-          AJ.push(relatedArtists12[0][i].id);
-        }
-        if(!AJ.includes(relatedArtists12[1][i].id)) {
-          AJ.push(relatedArtists12[1][i].id);
-        }
-      }
-      artistsAJId = AJ.slice(0, 10);
-      console.log('got id of 10 unique');
-    })
-    .done(function(){
-      artistsAJId.forEach(function (id){
-        $.ajax({
-          url: `https://api.spotify.com/v1/artists/${id}/related-artists`,
-          headers: {
-            'Authorization': 'Bearer bd56804a81f3496e909d25ae1ee01481',
-          },
-          async: false,
-          success: function (response) {
-            relatedArtistsAJ.push(response.artists);
-            console.log('got related artists of 10 unique');
-          }
-        })
-      })
-    })
-    .done(function(){
-      relatedArtistsAJ.forEach(function(element) {
-        let truncated = element.slice(0,5);
-        truncated.map(function(artist) {
-          artistsA1J5Id.push(artist.id);
-        })
-      })
-      console.log('pushing ids of 50 artists');
-    })
-    .done(function(){
-      artist12Id.forEach(function(id){
-        if (!allId.includes(id)){
-          allId.push(id);
-        }
-      })
-
-      artistsAJId.forEach(function(id){
-        if (!allId.includes(id)){
-          allId.push(id);
-        }
-      })
-
-      artistsA1J5Id.forEach(function(id){
-        if (!allId.includes(id)){
-          allId.push(id);
-        }
-      })
-    })
-    .done(function(){
-      allId.forEach(function(id) {
-        $.ajax({
-          url: `https://api.spotify.com/v1/artists/${id}/top-tracks?country=US`,
-          headers: {
-            'Authorization': 'Bearer bd56804a81f3496e909d25ae1ee01481',
-          },
-          async: false,
-          success: function (response) {
-            if (response.tracks[0]) {
-              trackData.push( response.tracks[0]);
-            }
-          }
-        })
-      })
-      console.log('got track data for final playlist');
-    }).done(function(){
-      spotifyView.displayPlaylist();
-      createPlaylist.pushUri();
-      console.log('ajax call complete');
-      page.redirect('/results');
-    })
+    // .done(artist12Id.forEach(function (id){
+    //   $.ajax({
+    //     url: `https://api.spotify.com/v1/artists/${id}/related-artists`,
+    //     headers: {
+    //       'Authorization': 'Bearer bd56804a81f3496e909d25ae1ee01481',
+    //     },
+    //     async: false,
+    //     success: function (response) {
+    //       relatedArtists12.push(response.artists);
+    //       console.log('got related artists of original 2');
+    //     }
+    //   })
+    // }))
+    // .done(function(){
+    //   for (let i = 0; i < 10; i++) {
+    //     if(!AJ.includes(relatedArtists12[0][i].id)) {
+    //       AJ.push(relatedArtists12[0][i].id);
+    //     }
+    //     if(!AJ.includes(relatedArtists12[1][i].id)) {
+    //       AJ.push(relatedArtists12[1][i].id);
+    //     }
+    //   }
+    //   artistsAJId = AJ.slice(0, 10);
+    //   console.log('got id of 10 unique');
+    // })
+    // .done(function(){
+    //   artistsAJId.forEach(function (id){
+    //     $.ajax({
+    //       url: `https://api.spotify.com/v1/artists/${id}/related-artists`,
+    //       headers: {
+    //         'Authorization': 'Bearer bd56804a81f3496e909d25ae1ee01481',
+    //       },
+    //       async: false,
+    //       success: function (response) {
+    //         relatedArtistsAJ.push(response.artists);
+    //         console.log('got related artists of 10 unique');
+    //       }
+    //     })
+    //   })
+    // })
+    // .done(function(){
+    //   relatedArtistsAJ.forEach(function(element) {
+    //     let truncated = element.slice(0,5);
+    //     truncated.map(function(artist) {
+    //       artistsA1J5Id.push(artist.id);
+    //     })
+    //   })
+    //   console.log('pushing ids of 50 artists');
+    // })
+    // .done(function(){
+    //   artist12Id.forEach(function(id){
+    //     if (!allId.includes(id)){
+    //       allId.push(id);
+    //     }
+    //   })
+    //
+    //   artistsAJId.forEach(function(id){
+    //     if (!allId.includes(id)){
+    //       allId.push(id);
+    //     }
+    //   })
+    //
+    //   artistsA1J5Id.forEach(function(id){
+    //     if (!allId.includes(id)){
+    //       allId.push(id);
+    //     }
+    //   })
+    // })
+    // .done(function(){
+    //   allId.forEach(function(id) {
+    //     $.ajax({
+    //       url: `https://api.spotify.com/v1/artists/${id}/top-tracks?country=US`,
+    //       headers: {
+    //         'Authorization': 'Bearer bd56804a81f3496e909d25ae1ee01481',
+    //       },
+    //       async: false,
+    //       success: function (response) {
+    //         if (response.tracks[0]) {
+    //           trackData.push( response.tracks[0]);
+    //         }
+    //       }
+    //     })
+    //   })
+    //   console.log('got track data for final playlist');
+    // }).done(function(){
+    //   spotifyView.displayPlaylist();
+    //   createPlaylist.pushUri();
+    //   console.log('ajax call complete');
+    //   page.redirect('/results');
+    // })
   }
   module.spotify = spotify;
   module.artist1artist2 = artist1artist2;
